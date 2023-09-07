a63804e77f6ddc8c9f75bff996a50cb0a73113f0
rafael.lumang <lumang.rafael@gmail.com>
2023-09-06 20:47:17 -0700

    added shirt color + design function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,49 @@
 console.log('test');
+
+// Add focus to name input
+const name = document.querySelector('#name');
+name.focus();
+
+// Program input field to display only when job role is 'other'
+const otherJobRole = document.querySelector('#other-job-role')
+const jobRole = document.querySelector('#title');
+
+
+jobRole.addEventListener("change", () => {
+    jobRoleValue = jobRole.value.toUpperCase();
+    if (jobRoleValue != "OTHER") {
+        otherJobRole.style.display = "none";
+    } else {
+        otherJobRole.style.display = "block"
+    }
+})
+
+// shirt colors appear according to shirt design
+
+const shirtColors = document.querySelector('#color');
+shirtColors.disabled = true;
+
+const shirtDesign = document.querySelector('#design');
+shirtDesign.addEventListener("change", () => {
+    let shirtDesignValue = shirtDesign.value.toUpperCase();
+    console.log(shirtDesignValue);
+    if (shirtDesignValue == "JS PUNS") {
+        console.log('js puns worked')
+        shirtColors.disabled = false;
+
+        const heartsJs = document.querySelectorAll('[data-theme="heart js"]');
+        heartsJs.forEach((heartJs) => {
+            heartJs.disabled = true;
+        })
+
+    } else if (shirtDesignValue == "HEART JS") {
+        shirtColors.disabled = false;
+        console.log('heart js worked')
+
+        const jsPuns = document.querySelectorAll('[data-theme="js puns"]');
+
+        jsPuns.forEach((jsPun) =>
+            jsPun.disabled = true);
+    }
+})
+
