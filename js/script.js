6f07e09c1885c52b8355dbc23a0d5bc4e8a12b26
rafael.lumang <lumang.rafael@gmail.com>
2023-09-07 19:05:04 -0700

    Added T-shirt design and color functionality

--- js/script.js
+++ js/script.js
@@ -23,27 +23,52 @@ jobRole.addEventListener("change", () => {
 const shirtColors = document.querySelector('#color');
 shirtColors.disabled = true;
 
+const shirtColorsOptions = document.querySelectorAll ('#color option')
+
+
+// const jsPuns = document.querySelectorAll('[data-theme="js puns]');
+// const heartsJS = document.querySelectorAll('[data-theme="heart js"]');
+
+// const shirtColors = document.querySelectorAll('#color');
+
+
 const shirtDesign = document.querySelector('#design');
+// let shirtDesignValue = shirtDesign.value.toUpperCase();
 shirtDesign.addEventListener("change", () => {
-    let shirtDesignValue = shirtDesign.value.toUpperCase();
-    console.log(shirtDesignValue);
-    if (shirtDesignValue == "JS PUNS") {
-        console.log('js puns worked')
-        shirtColors.disabled = false;
-
-        const heartsJs = document.querySelectorAll('[data-theme="heart js"]');
-        heartsJs.forEach((heartJs) => {
-            heartJs.disabled = true;
-        })
-
-    } else if (shirtDesignValue == "HEART JS") {
-        shirtColors.disabled = false;
-        console.log('heart js worked')
-
-        const jsPuns = document.querySelectorAll('[data-theme="js puns"]');
-
-        jsPuns.forEach((jsPun) =>
-            jsPun.disabled = true);
+    const shirtDesignValue = shirtDesign.value;
+    shirtColors.disabled = false;
+    shirtColorsOptions.forEach((shirtColorsOption) => {
+    const dataTheme = shirtColorsOption.getAttribute('data-theme');
+    if (dataTheme === shirtDesignValue) {
+        shirtColorsOption.style.display = "block";
+    } else {
+        shirtColorsOption.style.display = "none";
     }
-})
+    }
+)})
+// const shirtDesign = document.querySelector('#design');
+// shirtDesign.addEventListener("change", () => {
+//     let shirtDesignValue = shirtDesign.value.toUpperCase();
+//     console.log(shirtDesignValue);
+//     if (shirtDesignValue == "JS PUNS") {
+//         console.log('js puns worked')
+//         shirtColors.disabled = false;
+
+//         const heartsJs = document.querySelectorAll('[data-theme="heart js"]');
+//         heartsJs.forEach((heartJs) => {
+//             heartJs.disabled = true;
+//         })
+
+//     } else if (shirtDesignValue == "HEART JS") {
+//         shirtColors.disabled = false;
+//         console.log('heart js worked')
+
+//         const jsPuns = document.querySelectorAll('[data-theme="js puns"]');
+
+//         jsPuns.forEach((jsPun) =>
+//             jsPun.disabled = true);
+//     }
+// })
+
+
 
